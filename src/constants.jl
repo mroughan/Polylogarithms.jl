2738ed47edfa68c10931808501a2e7d72ca94129
Matthew Roughan <matthew.roughan@adelaide.edu.au>
2022-09-20 18:34:30 +0930

    went over to using the sensible package for irrational constants instead of Base, which fixes a precompile warning

--- src/constants.jl
+++ src/constants.jl
@@ -1,4 +1,5 @@
 # get hold of all the predefined math constants in Julia
 import Base.MathConstants: π, pi, ℯ, e, γ, eulergamma, catalan, φ, golden
-import IrrationalConstants: twoπ
+import IrrationalConstants: twoπ, log2π
+# prolly should convert some code to use the latter
 
