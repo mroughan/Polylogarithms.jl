69070458132bc7495aecf55d18fbbd466cdfd3a9
Matthew Roughan <matthew.roughan@adelaide.edu.au>
2025-11-12 14:58:37 +1030

    modified help for main function

--- src/polylog.jl
+++ src/polylog.jl
@@ -39,16 +39,16 @@ end
 
 Calculates the Polylogarithm function ``{Li}_s(z)`` defined by
     
-``{Li}_s = \\sum_{n=1}^{\\infty} \\frac{z^n}{n^s}``
+``{Li}_s = \\sum_{n=1}^{\\infty} \\frac{z^n}{n^s},``
 
-Uses double precision complex numbers (not arbitrary precision).
+or by analytic expension to the complex plane. 
+
+It double precision complex numbers (not arbitrary precision).
 It's goal is an relative error bound 10^{-12}.
  
 ## Input Arguments
 * ``s`` `::Complex`: the 'fractional' parameter
 * ``z`` `::Complex`: the point at which to calculate it
-
-It should also accept input arguments as `Real` or `Rational` or `Integer` but these aren't completely tested.
 
 There are additional keywords, but these are currently intended for testing not use.
 
@@ -75,20 +75,18 @@ end
     polylog(s, z, Diagnostics())
 
 Calculates the Polylogarithm function ``{Li}_s(z)`` defined by
+    
+``{Li}_s = \\sum_{n=1}^{\\infty} \\frac{z^n}{n^s},``
 
-``{Li}_s = \\sum_{n=1}^{\\infty} \\frac{z^n}{n^s}``
+or by analytic expension to the complex plane. 
 
-Uses double precision complex numbers (not arbitrary precision).
+It double precision complex numbers (not arbitrary precision).
 It's goal is an relative error bound 10^{-12}.
  
 ## Input Arguments
 * ``s::`` `Complex`: the 'fractional' parameter
 * ``z`` `::Complex`: the point at which to calculate it
 * `::Diagnostics`: use this to indicate that the output should include extra information
-
-It should also accept input arguments as `Real` or `Rational` or `Integer` but these aren't completely tested.
-
-There are additional keywords, but these are currently intended for testing not use.
 
 ## Output Arguments
 * ``Li_s(z)``: The result
